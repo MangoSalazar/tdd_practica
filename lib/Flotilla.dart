4ebd3d9e8a1dc14676a135fa7c4cbeafd05cfd67
Manguito <l22440193@mochis.tecnm.mx>
2025-09-25 14:39:43 -0700

    Corregi errores

--- lib/Flotilla.dart
+++ lib/Flotilla.dart
@@ -11,23 +11,51 @@ class Barco {
   TiposBarcos tipo;
   final DireccionesHacia direccion;
   final Punto puntoInicial;
-  List<Elemento> _elemento = [];
+  List<Elemento> _elementos = [];
   Barco({
     required this.tipo,
     required this.puntoInicial,
     required this.direccion,
-    required List<Elemento> elemento,
-  }) : _elemento = elemento;
+  }) {
+    int cuantasVeces = mapaTamanos[tipo]!;
+    int columna = puntoInicial.columna;
+    int fila = puntoInicial.fila;
+    while (cuantasVeces > 0) {
+      _elementos.add(Elemento(punto: Punto(columna: columna, fila: fila)));
+      columna = columna + dColumna[direccion]!;
+      fila = fila + dFila[direccion]!;
+      cuantasVeces--;
+    }
+  }
 }
+
+var mapaTamanos = {
+  TiposBarcos.bote: 1,
+  TiposBarcos.lancha: 2,
+  TiposBarcos.submarino: 3,
+  TiposBarcos.crucero: 4,
+  TiposBarcos.portaaviones: 5,
+};
+var dFila = {
+  DireccionesHacia.arriba: -1,
+  DireccionesHacia.abajo: 1,
+  DireccionesHacia.izquierda: 0,
+  DireccionesHacia.derecha: 0,
+};
+
+var dColumna = {
+  DireccionesHacia.arriba: 0,
+  DireccionesHacia.abajo: 0,
+  DireccionesHacia.izquierda: -1,
+  DireccionesHacia.derecha: 1,
+};
 
 class Flotilla {
   List<Barco> _barcos;
   int get cantidad => _barcos.length;
-  Flotilla(List<Barco> this._barcos) {
-    if (ahiRepetidos(_barcos)) throw FlotillaCantidadExcepcion;
-  }
-  bool ahiRepetidos(List<Barco> barcos) {
-    return barcos.map((e) => e.tipo).toSet().length != 5;
+  Flotilla(this._barcos) {
+    if (_barcos.length != 5) throw FlotillaCantidadExcepcion;
+    if (true) throw FlotillaTipos();
   }
 }
 
--- test/Flotilla_test.dart
+++ test/Flotilla_test.dart
@@ -4,15 +4,32 @@ import 'package:test/test.dart';
 import 'package:tdd_practica/Flotilla.dart';
 
 void main() {
-  test('Debe tener 5 tipos de barcos', () async {
-    var numBarcos = [
-      Barco(
-        tipo: TiposBarcos.bote,
-        puntoInicial: Punto(columna: 1, fila: 1),
-        direccion: DireccionesHacia.izquierda,
-        elemento: 1,
-      ),
-    ];
-    expect(() => Flotilla(numBarcos), throwsA(isA<FlotillaTipos>()));
+  var barcosIncorrectos = [
+    Barco(
+      tipo: TiposBarcos.bote,
+      puntoInicial: Punto(columna: 1, fila: 1),
+      direccion: DireccionesHacia.izquierda,
+    ),
+    Barco(
+      tipo: TiposBarcos.lancha,
+      puntoInicial: Punto(columna: 2, fila: 1),
+      direccion: DireccionesHacia.izquierda,
+    ),
+    Barco(
+      tipo: TiposBarcos.submarino,
+      puntoInicial: Punto(columna: 3, fila: 1),
+      direccion: DireccionesHacia.izquierda,
+    ),
+    Barco(
+      tipo: TiposBarcos.crucero,
+      puntoInicial: Punto(columna: 4, fila: 1),
+      direccion: DireccionesHacia.izquierda,
+    ),
+  ];
+  test('Debe tener 5 barcos', () {
+    expect(
+      () => Flotilla(barcosIncorrectos),
+      throwsA(isA<FlotillaCantidadExcepcion>()),
+    );
   });
 }
